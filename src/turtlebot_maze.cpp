f0953b0f40fa3e2984830cb15ed5d9a5a8711b9a
Kasper Woiceshyn <kasper.woiceshyn@asirobots.com>
2020-10-21 21:21:37 -0600

    Basic wall follower that follows a wall the left

---
+++ src/turtlebot_maze.cpp
@@ -0,0 +1,166 @@
+#include "turtlebot_maze.h"
+#include "pid.h"
+
+#include <csignal>
+#include <sstream>
+
+TurtleBotMaze::TurtleBotMaze(ros::NodeHandle& nh)
+        :nh_(nh),
+         loop_rate_(new ros::Rate(10)),
+         heading_error_(0),
+         center_range_(1.0)
+{
+    this->init();
+}
+
+void TurtleBotMaze::init(){
+    ROS_INFO("Initializing publishers and subscribers");
+    vel_publisher_ = nh_.advertise<geometry_msgs::Twist>("cmd_vel_mux/input/navi", 10);
+    laser_subscriber_ = nh_.subscribe("/laserscan", 1000, &TurtleBotMaze::callback_laser, this);
+    teleporter_ = nh_.serviceClient<gazebo_msgs::SetModelState>("/gazebo/set_model_state");
+
+    geometry_msgs::Point init_position;
+    init_position.x = 0.0;
+    init_position.y = 0.0;
+    init_position.z = 0.0;
+
+    geometry_msgs::Quaternion init_orientation;
+    init_orientation.x = 0.0;
+    init_orientation.y = 0.0;
+    init_orientation.z = 0.0;
+    init_orientation.w = 1.0;
+
+    geometry_msgs::Pose init_pose;
+    init_pose.position = init_position;
+    init_pose.orientation = init_orientation;
+
+    gazebo_msgs::ModelState init_model_state;
+    init_model_state.model_name = "mobile_base";
+    init_model_state.pose = init_pose;
+
+    gazebo_msgs::SetModelState srv;
+    srv.request.model_state = init_model_state;
+
+    if(teleporter_.call(srv)) {
+        ROS_INFO("Moved to start");
+    }
+    else {
+        ROS_ERROR("Failed move to start:%s",srv.response.status_message.c_str());
+    }
+    //TODO: Re-init the hokuyo?
+}
+
+void TurtleBotMaze::follow_wall(){
+    geometry_msgs::Twist move_cmd;
+    move_cmd.linear.x = 0.2;
+    while(ros::ok()) {
+        move_cmd.angular.z = k_*heading_error_; //simple P-controller
+        ROS_INFO("Ang cmd: %f", move_cmd.angular.z);
+        vel_publisher_.publish(move_cmd);
+        ros::spinOnce();
+        loop_rate_->sleep();
+        if(center_range_ < 0.5){ //too close to wall in front
+            stop();
+            if(left_range_ < right_range_)
+                rotate_angle(-M_PI_2);
+            else
+                rotate_angle(M_PI_2);
+        }
+    }
+}
+
+void TurtleBotMaze::stop(){
+    geometry_msgs::Twist move_cmd;
+    move_cmd.linear.x = 0.0;
+    move_cmd.angular.z = 0.0;
+    vel_publisher_.publish(move_cmd);
+}
+
+void TurtleBotMaze::rotate_angle(float angle_in) {
+    geometry_msgs::Twist move_cmd;
+    move_cmd.linear.x = 0.0;
+    if(angle_in > 0)
+        move_cmd.angular.z = 0.1;
+    else
+        move_cmd.angular.z = -0.1;
+    float angle_current = 0;
+    while(std::abs(angle_current) < std::abs(angle_in)){
+        vel_publisher_.publish(move_cmd);
+        loop_rate_->sleep();
+        angle_current += move_cmd.angular.z*0.1;
+        ROS_INFO("Rotating: %f", angle_current);
+    }
+}
+
+void TurtleBotMaze::callback_laser(const sensor_msgs::LaserScan &msg) {
+    left_range_ = msg.ranges[left_laser_idx_];
+    right_range_ = msg.ranges[right_laser_idx_];
+    float lf_range = msg.ranges[lf_laser_idx_];
+    //when robot is parallel to wall this ratio corresponds to a 20 deg angle
+    float actual_ratio = left_range_ / lf_range;
+    heading_error_ = desired_ratio_ - actual_ratio; //not actually an angle
+    center_range_ = msg.ranges[center_laser_idx_];
+    ROS_INFO("Heading error: %f", heading_error_);
+    current_scan_ = msg;
+}
+/*
+void TurtleBotMaze::update_walls(){
+    //simple split of left and right sides for now
+    std::array<float,180> ranges;
+    std::array<float,180> bearings;
+    const float eps = 0.1;
+    const size_t count_thresh = 30;
+
+    float rhoi = current_scan_.ranges[0];
+    float thetai = 0; //angle from where?
+    float a_num1 = rhoi*rhoi*sin(2*thetai);
+    float a_den1 = rhoi*rhoi*cos(2*thetai);
+    float a_num2 = 0, a_den2 = 0;
+    size_t counter = 1;
+    for(size_t i = 1; i < 180; ++i){
+        //dont forget to check discrepancy
+        rhoi = current_scan_.ranges[i];
+        if(std::abs(rhoi - current_scan_.ranges[i]) < eps){
+            thetai = current_scan_.angle_increment*i; //angle from where?
+            a_num1 += rhoi*rhoi*sin(2*thetai);
+            a_den1 += rhoi*rhoi*cos(2*thetai);
+            for(size_t j = 0; j < i; ++j){
+                float rhoj = current_scan_.ranges[j];
+                float thetaj = current_scan_.angle_increment*j;
+                a_num2 += rhoi*rhoj*cos(thetai)*sin(thetaj);
+                a_den2 += rhoi*rhoj*cos(thetai + thetaj);
+            }
+            ++counter;
+        }
+
+    }
+
+    //loop through left side points
+    //if range - previous range > threshold, split into new group
+    // dont use ranges that are within say 95% of max
+    //compute fit parameters on current group and store
+
+    //store wall parameters as nx2, where n is number of walls
+    //r and a, where r is min dist of wall to laser, a is its bearing
+
+}*/
+
+void mySigintHandler(int sig)
+{
+    ROS_INFO("Stopping turtlebot");
+    ros::shutdown();
+}
+
+
+int main(int argc, char **argv)
+{
+
+    ros::init(argc, argv, "turtlebot_maze", ros::init_options::NoSigintHandler);
+    ros::NodeHandle nh;
+    signal(SIGINT, mySigintHandler);
+
+    TurtleBotMaze tb_maze(nh);
+    tb_maze.follow_wall();
+
+    return 0;
+}
---
+++ src/turtlebot_maze.h
@@ -0,0 +1,40 @@
+#ifndef TURTLEBOT_MAZE_TURTLEBOT_MAZE_H
+#define TURTLEBOT_MAZE_TURTLEBOT_MAZE_H
+
+#include <ros/ros.h>
+#include <std_msgs/String.h>
+#include <geometry_msgs/Twist.h>
+#include <sensor_msgs/LaserScan.h>
+#include <nav_msgs/Odometry.h>
+#include <gazebo_msgs/SetModelState.h>
+
+class TurtleBotMaze{
+public:
+    TurtleBotMaze(ros::NodeHandle& nh);
+    void init();
+    void follow_wall();
+    void stop();
+    void rotate_angle(float angle_in);
+    void callback_laser(const sensor_msgs::LaserScan& msg);
+    //void update_walls();
+
+private:
+    const size_t left_laser_idx_ = 359;
+    const size_t right_laser_idx_ = 0;
+    const size_t center_laser_idx_= 179;
+    const size_t lf_laser_idx_ = 319; //20 deg forward from left laser
+    const float k_ = 1.0;
+    const float desired_ratio_ = cos(0.349); //20 deg
+    float heading_error_;
+    float center_range_, left_range_, right_range_;
+    ros::NodeHandle nh_;
+    ros::Publisher vel_publisher_;
+    ros::Subscriber laser_subscriber_;
+    ros::Rate* loop_rate_;
+
+    ros::ServiceClient teleporter_;
+    sensor_msgs::LaserScan current_scan_;
+};
+
+
+#endif //TURTLEBOT_MAZE_TURTLEBOT_MAZE_H
